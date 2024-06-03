7a5c836e0cfc68f12f73eb2a2f4b32d660b0c1c1
Mohamed Mousa <mohamedmousa11520031@gmail.com>
2024-06-03 20:39:10 +0300

    first project

---
+++ lib/core/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const Color blacColor = Color(0xff1E1E1E);
+const Color whiteColor = Color(0xffFFFFFF);
+const Color iconColor = Color(0xff8875FF);
+const Color redColor = Color(0xffFF4666);
+const Color yellowColor = Color(0xffCC8441);
